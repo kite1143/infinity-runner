3a801104b2d56f2800f835f6417a8c6fd0d13687
kite1143 <70432793+kite1143@users.noreply.github.com>
2026-03-15 18:07:13 +0700

    09. More assets and Reorganize folders
    Add more assets for world buidings and reorganizing assets folder

---
+++ WorldBuilding/Enemies/runner_skeleton.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var speed: float = 10
+@export var trigger_range: Area3D
+
+@onready var animation_player: AnimationPlayer = $Skeleton_Warrior/AnimationPlayer
+
+var is_running: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if trigger_range:
+		trigger_range.body_entered.connect(player_entered)
+
+func _process(delta: float) -> void:
+	if is_running:
+		global_position.z += delta * speed
+
+func player_entered(body: Player) -> void:
+	is_running = true
+	animation_player.play("Running_HoldingRifle")
