7414a58b43c05be2cf1e9a14569d93f2bbfabfac
kite1143 <70432793+kite1143@users.noreply.github.com>
2026-06-10 14:36:49 +0700

    54. Add difficulty curve

--- Scenes/main_game.gd
+++ Scenes/main_game.gd
@@ -4,6 +4,8 @@ var player: Player
 @onready var spawn_pos: Marker3D = $PlayerSpawn
 @onready var chunk_generator: ChunkGenerator = $ChunkGenerator
 
+@export var difficult_curve: Curve
+
 func _ready() -> void:
 	SoundManager.play_gameplay_music()
 	player = (DataManager.list_characters[
@@ -19,10 +21,4 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	GameManager.score = -player.global_position.z + 2*GameManager.coin
 	var distance: float = -player.global_position.z
-	if distance > 72*10:
-		GameManager.time_scale = 1.2
-	elif distance > 72*20:
-		GameManager.time_scale = 1.4
-	elif distance > 72*40:
-		GameManager.time_scale = 1.5
-	Engine.time_scale = GameManager.time_scale
+	Engine.time_scale = difficult_curve.sample(distance)
